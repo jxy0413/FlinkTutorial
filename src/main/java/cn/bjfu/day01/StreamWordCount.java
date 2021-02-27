94e4d962ac60436caa6aa55a0cfd416d3f367577
jxy0413 <15011006250@163.com>
2021-02-27 10:57:21 +0800

    wordCount

--- src/main/java/cn/bjfu/day01/StreamWordCount.java
+++ src/main/java/cn/bjfu/day01/StreamWordCount.java
@@ -1,14 +1,21 @@
 package cn.bjfu.day01;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class StreamWordCount {
     public static void main(String[] args)throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+//        env.setParallelism(1);
 
-        String inputPath = "C:\\Users\\Administrator\\IdeaProjects\\FlinkTutorial\\src\\main\\resources\\hello.txt" ;
-        DataStreamSource<String> inputStream = env.readTextFile(inputPath);
+//        String inputPath = "C:\\Users\\Administrator\\IdeaProjects\\FlinkTutorial\\src\\main\\resources\\hello.txt" ;
+////        DataStreamSource<String> inputStream = env.readTextFile(inputPath);
+        //用Flink parameter tool工具从程序启动参数中提取配置项
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+        DataStream<String> inputStream = env.socketTextStream(host,port);
 
         inputStream.flatMap(new WordCount.MyflatMapper()).keyBy(0).sum(1).print();
 
